959d00b398652b30dc2139b15e9ff03b89792007
CenturyNine <lowlifeix@gmail.com>
2023-03-21 18:36:48 +0700

    up

--- NodeJS-1/main.py
+++ NodeJS-1/main.py
@@ -1,11 +1,14 @@
 from rembg import remove
 from PIL import Image
+import easygui as eg
 import sys
  
 input_path = sys.argv[1]
 output_path = sys.argv[2]
- 
-input = Image.open(input_path)
-output = remove(input)
 
+input = Image.open(input_path)
+output_path_savepc = eg.filesavebox(title='Save file to..')
+
+output = remove(input)
+output.save(output_path_savepc)
 output.save(output_path)
